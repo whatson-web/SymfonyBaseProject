9e910a1c1d8eb8d6ecb53b1feef981ba3e6c7539
JF <jf@whatson-web.com>
2016-12-19 16:13:41 +0100

    composer update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,6 +9,12 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 class AppKernel extends Kernel
 {
 
+	public function __construct($environment, $debug)
+	{
+		parent::__construct($environment, $debug);
+		date_default_timezone_set('Europe/Paris');
+	}
+
 	public function registerBundles()
 	{
 		$bundles = array(
