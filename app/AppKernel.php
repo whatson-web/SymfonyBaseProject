a32f7ea39a0ca2c03fb93f52d0835cd1d71488b1
JF <jf@whatson-web.com>
2018-05-17 14:37:36 +0200

    composer update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -9,62 +9,62 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 class AppKernel extends Kernel
 {
 
-	public function __construct($environment, $debug)
-	{
-		parent::__construct($environment, $debug);
-		date_default_timezone_set('Europe/Paris');
-	}
+    public function __construct($environment, $debug)
+    {
+        parent::__construct($environment, $debug);
+        date_default_timezone_set('Europe/Paris');
+    }
 
-	public function registerBundles()
-	{
-		$bundles = array(
-			new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-			new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-			new Symfony\Bundle\TwigBundle\TwigBundle(),
-			new Symfony\Bundle\MonologBundle\MonologBundle(),
-			new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-			new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-			new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-			new WH\LibBundle\WHLibBundle(),
-			new WH\BackendTemplateBundle\WHBackendTemplateBundle(),
-			new WH\BackendBundle\WHBackendBundle(),
-			new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-			new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
-			new WH\SuperAdminBundle\WHSuperAdminBundle(),
-			new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-			new WH\SeoBundle\WHSeoBundle(),
-			new Oneup\FlysystemBundle\OneupFlysystemBundle(),
-			new FM\ElfinderBundle\FMElfinderBundle(),
-			new WH\MediaBundle\WHMediaBundle(),
-			new FOS\UserBundle\FOSUserBundle(),
-			new WH\UserBundle\WHUserBundle(),
-			new cspoo\Swiftmailer\MailgunBundle\cspooSwiftmailerMailgunBundle(),
-			new Http\HttplugBundle\HttplugBundle(),
-			new WH\ParameterBundle\WHParameterBundle(),
-			new MainBundle\MainBundle(),
-			new BackendBundle\BackendBundle(),
+    public function registerBundles()
+    {
+        $bundles = [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new WH\LibBundle\WHLibBundle(),
+            new WH\BackendTemplateBundle\WHBackendTemplateBundle(),
+            new WH\BackendBundle\WHBackendBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Stfalcon\Bundle\TinymceBundle\StfalconTinymceBundle(),
+            new WH\SuperAdminBundle\WHSuperAdminBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new WH\SeoBundle\WHSeoBundle(),
+            new Oneup\FlysystemBundle\OneupFlysystemBundle(),
+            new FM\ElfinderBundle\FMElfinderBundle(),
+            new WH\MediaBundle\WHMediaBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new WH\UserBundle\WHUserBundle(),
+            new cspoo\Swiftmailer\MailgunBundle\cspooSwiftmailerMailgunBundle(),
+            new Http\HttplugBundle\HttplugBundle(),
+            new WH\ParameterBundle\WHParameterBundle(),
+            new MainBundle\MainBundle(),
+            new BackendBundle\BackendBundle(),
             new UserBundle\UserBundle(),
             new Symfony\Bundle\WebServerBundle\WebServerBundle(),
             new CmsBundle\CmsBundle(),
             new WH\CmsBundle\WHCmsBundle(),
             new ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+        ];
 
-        );
+        if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+        }
 
-		if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
-			$bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-			$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-			$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-			$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-			$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-		}
+        return $bundles;
+    }
 
-		return $bundles;
-	}
-
-	public function registerContainerConfiguration(LoaderInterface $loader)
-	{
-		$loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
-	}
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
 }
